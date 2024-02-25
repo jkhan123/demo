b842ede147d195c842d5c5874472e78fe610a3d6
Janealam <79janealam@gmail.com>
2024-02-25 16:37:39 +0530

    Add files via upload
    added files

---
+++ arithmetic_operation.py
@@ -0,0 +1,12 @@
+a=int(input("Enter first integer value: "))
+b=int(input("Enter second integer value: "))
+add=a+b
+sub=b-a
+mul=a*b
+div=b/a
+rem=b%a
+print("Addition of two numbers: ",add)
+print("Difference of two numbers: ",sub)
+print("Product of two numbers: ",mul)
+print("Division of two numbers: ",div)
+print("Remainder of two numbers: ",rem)
